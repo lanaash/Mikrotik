14f7d5d5e9fc011302df30b622bd619fde72b3eb
lanaash <2alanhand@googlemail.com>
2025-02-03 10:02:15 +0000

    Update upgradeMikrotik.sh

--- upgradeMikrotik.sh
+++ upgradeMikrotik.sh
@@ -5,7 +5,9 @@
 # The approved routeros version is stored for each architecture in routeros/upgrade/$ARCHITECTURE/
 # e.g. routeros/upgrade/arm/routeros-7.17.1-arm.npk
 #
-# Put a list of Mikrotik CPE IP/FQDNs into file called ips.txt
+# Put a list of Mikrotik IP/FQDNs into file called ips.txt
+#
+# ASH 2025
 #-------------------------------------------------------------------------------------------------------
 
 echo ; echo "* Mikrotik upgrade script" ; echo
@@ -13,7 +15,6 @@ echo ; echo "* Mikrotik upgrade script" ; echo
 # Vars
 APIUSER=apiuser
 SSHUSER=admin
-DEBUG=True
 
 # Get passwords
 echo "Mikrotik admin password:"
@@ -24,9 +25,11 @@ read -s APIPASS
 # Read in router IP/FQDNs from file
 for ROUTER in `cat ips.txt`; do
 
-    # Flags
+    # Control vars
     IS_ONLINE=''
     IS_MIKROTIK=''
+    NO_UPGRADE_FILE=''
+    ERROR_MSG='Errors           ='
 
     echo; echo "** Connecting to $ROUTER"
 
@@ -38,12 +41,12 @@ for ROUTER in `cat ips.txt`; do
         if [ "$PLATFORM" = "mikrotik" ]; then
             IS_MIKROTIK=1
         else
-            echo " Not a mikrotik"
             IS_MIKROTIK=0
+            ERROR_MSG="${ERROR_MSG} Not a mikrotik. "
         fi
     else
-        echo " Offline"
         IS_ONLINE=0
+        ERROR_MSG="${ERROR_MSG} Not online. "
     fi
 
     if [ "$IS_ONLINE" = "1" ] && [ "$IS_MIKROTIK" = "1"  ]; then
@@ -54,33 +57,46 @@ for ROUTER in `cat ips.txt`; do
         # Get our current version number and filename
         UPGRADE_VERSION=$(ls -1t routeros/upgrade/$ARCHITECTURE/ |  head -n1 | sed 's/routeros-\(.*\)-.*/\1/')
         UPGRADE_FILE=$(ls -1t routeros/upgrade/$ARCHITECTURE/ | head -n1)
-
-        # Debug
-        if [ "$DEBUG" = "True" ]; then
-            echo " DEBUG: Router model     = $PLATFORM"
-            echo " DEBUG: RouterOS version = $CURRENT_VERSION"
-            echo " DEBUG: Architecture     = $ARCHITECTURE"
-            echo " DEBUG: Upgrade version  = $UPGRADE_FILE"
+        if [[ -z "$UPGRADE_FILE" ]]; then
+            # No file
+            NO_UPGRADE_FILE=0
+            ERROR_MSG="${ERROR_MSG} No upgrade firmware found. "
         fi
 
+        if [ "$UPGRADE_VERSION" = "$CURRENT_VERSION" ]; then
+             ERROR_MSG="${ERROR_MSG} Upgrade not required.  "
+        fi
+
+        if [[ "$CURRENT_VERSION" =~ ^6 ]]; then
+             ERROR_MSG="${ERROR_MSG} Not version 7. "
+        fi
+
+        echo " Router model     = $PLATFORM"
+        echo " RouterOS version = $CURRENT_VERSION"
+        echo " Architecture     = $ARCHITECTURE"
+        echo " Upgrade version  = $UPGRADE_VERSION"
+
+
         # Check that the current version is routeros 7 and also not running the upgrade version already
-        if [ "$UPGRADE_VERSION" != "$CURRENT_VERSION" ] && [[ "$CURRENT_VERSION" =~ ^7 ]]; then
+        if [ "$UPGRADE_VERSION" != "$CURRENT_VERSION" ] && [[ "$CURRENT_VERSION" =~ ^7 ]] && [ "$NO_UPGRADE_FILE" != "0" ]; then
             echo " Can be upgraded with routeros/upgrade/$ARCHITECTURE/$UPGRADE_FILE"
             read -p " Proceed to upgrade and reboot the router? (y or n) " -n 1 -r
             if [[ $REPLY =~ ^[Yy]$ ]]; then
                 echo ; echo " Copying $UPGRADE_FILE"
                 sshpass -p "$SSHPASS" scp routeros/upgrade/$ARCHITECTURE/$UPGRADE_FILE $SSHUSER@$ROUTER:/. > /dev/null 2>&1
                 if [ "$?" = "0" ]; then
-                    echo " Rebooting..."
+                    echo " File copied, router now rebooting to start upgrade!"
                     curl -k -u $APIUSER:$APIPASS -s -X POST http://$ROUTER/rest/system/reboot  > /dev/null 2>&1
                 else
                     echo " File copy failed!"
                 fi
             else
-                echo " Leaving this one then..."
+                echo " $ERROR_MSG"
             fi
         else
-            echo " Running our current version so nothing to do"
+            echo " $ERROR_MSG"
         fi
+    else
+        echo " $ERROR_MSG"
     fi
 done
